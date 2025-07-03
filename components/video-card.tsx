b15f3857491517ed3888163e3e5e96104547a409
Troy Moses Mugabi <mugabimoses07@gmail.com>
2025-07-03 16:20:55 +0300

    Update video-card.tsx

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -5,8 +5,6 @@ import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
 import { Play } from "lucide-react"
 import { VideoModal } from "./video-modal"
-import  YouTubeVideos from "@/components/YouTubeVideos";
-
 
 interface VideoCardProps {
   title: string
