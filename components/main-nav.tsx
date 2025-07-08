b0fdc6537f790d90c388718a85e232be85c91dd5
Daniel Marvin <itsupport@bugemauniv.ac.ug>
2025-07-08 18:00:55 +0300

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -76,7 +76,7 @@ export function MainNav() {
                 Sermons
               </Button>
             </Link>
-            <Link href="/Departments">
+            <Link href="/departments">
               <Button variant="ghost" className="text-[#2F557F] text-base">
                 Department
               </Button>
